4888182e2becfd287f4cc35bedbcab23841c9bc0
Shiraishi-Arata <119672885+Shiraishi-Arata@users.noreply.github.com>
2025-10-17 00:35:43 +0900

    Add Log class with static info logging method

---
+++ LWJGL/src/main/java/android/util/Log.java
@@ -0,0 +1,11 @@
+package android.util;
+
+public class Log {
+   public Log() {
+   }
+
+   public static int i(String var0, String var1) {
+      System.out.println(var0 + ": " + var1);
+      return 0;
+   }
+}
